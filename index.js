b89412bd9cfffed6399cbc42135ed02f55be1e22
anupamsingh007 <75076385+anupamsingh007@users.noreply.github.com>
2021-08-21 11:56:52 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+var readLineSync = require("readline-sync");
+var userName = readLineSync.question("Hi there! what is your name?");
+console.log("Welcome " + userName + " to 'do you know Anupam'");
+var score = 0;
+
+function game(question, answer) {
+    var userAnswer = readLineSync.question(question);
+    if (userAnswer === answer) {
+        console.log("You are right!");
+        score = score + 1;
+    } else {
+        console.log("you are wrong!");
+    }
+    console.log("score is: ", score);
+    console.log("------------------");
+}
+
+var questions = [{
+        question: "which city do i live in?",
+        answer: "dehradun"
+    },
+    {
+        question: "what is my favorite color?",
+        answer: "blue"
+    }
+]
+
+for (i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    game(currentQuestion.question, currentQuestion.answer);
+}
+console.log("Awesome! Your Total Score is: ", score);
